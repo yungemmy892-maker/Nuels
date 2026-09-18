309bb08906989a1b539a319bfe74e3dd87f04371
CodeWithNuel <yungemmy892@gmail.com>
2026-09-19 00:53:29 +0100

    Delete .vercel/output directory

--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.CL23_-S8.js","_app/immutable/chunks/qzX8Pj0D.js","_app/immutable/chunks/CfdghhU4.js","_app/immutable/chunks/ClIm2p38.js","_app/immutable/chunks/CJi-AoIC.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .vercel/output/static/_app/immutable/entry/start.CGQDNKsV.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/ClIm2p38.js";import{y as t}from"../chunks/CJi-AoIC.js";export{t as load_css,r as start};
